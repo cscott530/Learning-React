211f870a8a9c9cec70000faca86f29710be95da8
Chris Scott <cscott@problemsolutions.net>
2015-07-29 15:03:28 -0400

    removed unwanted files.

--- build/.module-cache/2ff4c6b644cef454ad740e783a937ee824a79afe.js
+++
@@ -1,9 +0,0 @@
-var UserProgress = React.createClass({displayName: "UserProgress",
-	render: function() {
-		return (
-			React.createElement("div", {className: "userProgress"}
-			)
-		);
-	}
-});
-React.render(React.createElement(UserProgress, null), $('#content')[0]);
--- build/.module-cache/fa0cd1509af4df2c98949932b1daf266b4e654cb.js
+++
@@ -1,4 +0,0 @@
-var CommentBox = React.createClass({displayName: "CommentBox",
-
-});
-React.render(React.createElement(CommentBox, {pollInterval: 500}), $('#content')[0]);
